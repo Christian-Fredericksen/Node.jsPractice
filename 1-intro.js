8bbc768e92cf6734936aa93bedb99dab52388fc5
christian_fredericksen <christian.fredericksen@yahoo.com>
2021-07-01 14:50:03 -0700

    first

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 20;
+if(amount < 10){
+    console.log("The amount is less than 10")
+}else if(amount == 10){
+    console.log("The amount is 10")
+}else {
+    console.log("The amount is more than 10")
+}
+console.log("This is my first app")
