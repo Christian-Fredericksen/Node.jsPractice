e93c4f91cd8f414458ca7127fcbad5c040146ba4
christian_fredericksen <christian.fredericksen@yahoo.com>
2021-07-27 13:51:20 -0700

    intro statement for the fs module

---
+++ app.js
@@ -0,0 +1,4 @@
+// NEXT WE WILL look at the fs --file system-- module. There are two approaches we can use with this
+// module. Asynchronously (non-blockin) or synchronously (blocking). Right now we will cover the setup
+// for both approaches. Later on we will discuss the differences between the two and when you might
+// choose one over the other. For now let's just think of it as two different methods.
